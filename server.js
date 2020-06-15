d2b289e2327d26ae35e274a3d041388ff1739828
rodrigoaalmeida <rodrigo_a.almeida@outlook.com>
2020-06-14 21:28:36 -0400

    Instalação do nunjucks e criação das rotas

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+
+const server = express()
+
+server.use(express.static('public'))
+
+server.set("view engine", "html")
+
+nunjucks.configure("views", {
+    express: server
+})
+
+server.get("/", function(req, res){
+    return res.render("index")
+})
+
+server.get("/receitas", function(req, res){
+    return res.render("receitas")
+})
+
+server.get("/sobre", function(req, res){
+    return res.render("sobre")
+})
+
+server.listen(5000, function(){
+    console.log("Server is running")
+})
